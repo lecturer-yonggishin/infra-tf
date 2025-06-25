2576f5a53d095535db59db76e662f6cd2e0e2885
lecturer.yonggishin <lecturer.yonggishin@gmail.com>
2025-06-25 12:28:55 +0900

    update vpc name

--- provider.tf
+++ provider.tf
@@ -5,12 +5,20 @@ terraform {
       version = ">= 5.0.0"
     }
   }
-  backend "s3" {
-    bucket         = "backend-bucket-ce2"
-    key            = "terraform/state-test/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "terraform-lock2"
-  }
+  cloud { 
+    
+    organization = "tfc-ce" 
+
+    workspaces { 
+      name = "infra-tf2" 
+    } 
+  } 
+  # backend "s3" {
+  #   bucket         = "backend-bucket-ce2"
+  #   key            = "terraform/state-test/terraform.tfstate"
+  #   region         = "ap-northeast-2"
+  #   dynamodb_table = "terraform-lock2"
+  # }
 }
 
 provider "aws" {
